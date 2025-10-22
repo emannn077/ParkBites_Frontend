fa62ace8c1751cd4ef61a89cf8b6d06094e09850
emannn077 <emanrauf779@gmail.com>
2025-10-22 14:41:58 +0300

    frontend chnages

--- pages/Welcome.jsx
+++ pages/Welcome.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Welcome = () => {
   return (
     <div className="welcome">
-      <h1>Welcome to SEB-X ParkBites </h1>
+      <h1>Welcome to SEB-X ParkBites !! </h1>
       <img src="/images/hungry.gif" alt="Hungry gif" className="welcome-gif" />
 
       <Link to="/trucks" className="start-button">
